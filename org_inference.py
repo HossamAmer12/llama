445b55e7b90b339a574d9c3894edd988c29b3013
Hossam Amer <hossam.amer@Hossams-MacBook-Pro.local>
2026-05-07 20:57:49 -0400

    small bug in org inference

--- org_inference.py
+++ org_inference.py
@@ -6,7 +6,7 @@ import json
 from sentencepiece import SentencePieceProcessor
 from tqdm import tqdm
 
-from model import ModelArgs, Transformer
+from org_model import ModelArgs, Transformer
 
 class LLaMA:
 
